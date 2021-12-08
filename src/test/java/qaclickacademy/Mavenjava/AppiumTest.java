b945f9ad6ea84c22cea9ca9a9fd71ab38126ba51
kajal <kajalv@zimetrics.com>
2021-12-08 15:15:15 +0530

    2nd time modified one push

--- src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -21,6 +21,19 @@ public class AppiumTest {
         System.out.println("new test added section 22");
     }
 
+    
+    @Test
+    public void Modified1() {
+        System.out.println("modified 3:13 wed 8 dec ");
+    }
+
+    @Test
+    public void modified2() {
+        System.out.println("modified 3:20 wed 8 dec");
+    }
+
+
+
 
 
 
